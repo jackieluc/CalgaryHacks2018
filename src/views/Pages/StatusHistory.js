4225ea8b93b177c70c1ad407fc4eda9c09de8abf
Jackie Luc <jaluc@ucalgary.ca>
2018-02-17 21:09:17 -0700

    Dummy status history page

---
+++ src/views/Pages/StatusHistory.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import HistoryChart from '../../components/HistoryChart';
+
+class StatusHistory extends Component {
+  render() {
+    return (
+      <HistoryChart />
+    );
+  }
+}
+
+export default StatusHistory;
