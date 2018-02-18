23fef522a9a51b7c59a2a12752253ea58ecf223d
Jackie Luc <jaluc@ucalgary.ca>
2018-02-17 17:49:28 -0700

    Added basic template for dashboard page

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Row, Col } from 'react-bootstrap';
 
 class IndexPage extends React.Component {
   constructor(props) {
@@ -9,7 +10,14 @@ class IndexPage extends React.Component {
   render() {
     return (
       <main>
-        <p>poop</p>
+        <Row style={{ height: '100vh', width: '100%', overflow: 'hidden', }}>
+          <Col xs={12} sm={9} style={{ backgroundColor: 'aqua', height: '100%', }}>
+            <div>Dashboard here</div>
+          </Col>
+          <Col xs={12} sm={3} style={{ backgroundColor: 'green', height: '100%', }}>
+            <div>Status info boxes here</div>
+          </Col>
+        </Row>
       </main>
     );
   }
